fb4f53483a1e0446f38be391e47e375371dedf3a
SergeySave <sergeysav.nn@gmail.com>
2017-01-11 19:31:03 -0500

    changes the transcript class to create its own spliterator

--- src/sergeysav/neuralnetwork/chess/Transcript.java
+++ src/sergeysav/neuralnetwork/chess/Transcript.java
@@ -2,8 +2,10 @@ package sergeysav.neuralnetwork.chess;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Spliterator;
+import java.util.function.Consumer;
 
-public class Transcript implements Iterable<double[]> {
+public class Transcript {
 	private LinkedList<String> moves = new LinkedList<String>();
 	private String outcome = "ERROR";
 
@@ -19,64 +21,79 @@ public class Transcript implements Iterable<double[]> {
 		return moves;
 	}
 
-	@Override
-	public Iterator<double[]> iterator() {
-		return new TranscriptIterator();
+	public Spliterator<double[]> spliterator() {
+		return new TranscriptSpliterator();
 	}
 
-	private class TranscriptIterator implements Iterator<double[]> {
-
+	private class TranscriptSpliterator implements Spliterator<double[]> {
 		private Iterator<String> moveIterator;
+		private int sizeLeft;
 		private ChessBoard board;
 		private boolean whiteMoving;
 
-		public TranscriptIterator() {
+		public TranscriptSpliterator() {
 			moveIterator = moves.iterator();
 			board = new ChessBoard();
 			whiteMoving = true;
+			sizeLeft = moves.size();
 		}
 
 		@Override
-		public boolean hasNext() {
-			return moveIterator.hasNext();
-		}
+		public boolean tryAdvance(Consumer<? super double[]> action) {
+			if (moveIterator.hasNext()) {
+				double[] output = new double[518]; // 6*8*8 input + 2*8*8+6 output
 
-		@Override
-		public double[] next() {
-			double[] output = new double[518]; // 6*8*8 input + 2*8*8+6 output
+				String move = moveIterator.next();
+				String[] parts = move.split(";");
 
-			String move = moveIterator.next();
-			String[] parts = move.split(";");
+				int fromRow = Integer.parseInt(parts[0].charAt(0) + "");
+				int fromCol = Integer.parseInt(parts[0].charAt(1) + "");
 
-			int fromRow = Integer.parseInt(parts[0].charAt(0) + "");
-			int fromCol = Integer.parseInt(parts[0].charAt(1) + "");
+				int toRow = Integer.parseInt(parts[1].charAt(0) + "");
+				int toCol = Integer.parseInt(parts[1].charAt(1) + "");
 
-			int toRow = Integer.parseInt(parts[1].charAt(0) + "");
-			int toCol = Integer.parseInt(parts[1].charAt(1) + "");
+				int piece = Integer.parseInt(parts[2]);
+				int pieceTypeMove = Math.abs(piece);
+				
+				System.arraycopy(board.generateNeuralInputs(whiteMoving), 0, output, 0, 384);
 
-			int piece = Integer.parseInt(parts[2]);
-			int pieceTypeMove = Math.abs(piece);
-			
-			System.arraycopy(board.generateNeuralInputs(whiteMoving), 0, output, 0, 384);
+				if (whiteMoving) { //Generate the data as if the white team is moving
+					//Add the outputs to the array
+					output[384 + fromRow * 8 + fromCol] = 1;
+					output[448 + toRow * 8 + toCol] = 1;
+					output[512 + pieceTypeMove - 1] = 1;
+				} else { //Generate the data as if the black team is moving
+					//Add the inputs to the array
+					//Add the outputs to the array
+					output[384 + (7-fromRow) * 8 + fromCol] = 1;
+					output[448 + (7-toRow) * 8 + toCol] = 1;
+					output[512 + pieceTypeMove - 1] = 1;
+				}
 
-			if (whiteMoving) { //Generate the data as if the white team is moving
-				//Add the outputs to the array
-				output[384 + fromRow * 8 + fromCol] = 1;
-				output[448 + toRow * 8 + toCol] = 1;
-				output[512 + pieceTypeMove - 1] = 1;
-			} else { //Generate the data as if the black team is moving
-				//Add the inputs to the array
-				//Add the outputs to the array
-				output[384 + (7-fromRow) * 8 + fromCol] = 1;
-				output[448 + (7-toRow) * 8 + toCol] = 1;
-				output[512 + pieceTypeMove - 1] = 1;
+				board.applyConvertedMove(move);
+
+				whiteMoving = !whiteMoving;
+				
+				action.accept(output);
+
+				return true;
 			}
+			return false;
+		}
 
-			board.applyConvertedMove(move);
+		@Override
+		public Spliterator<double[]> trySplit() {
+			return null;
+		}
 
-			whiteMoving = !whiteMoving;
+		@Override
+		public long estimateSize() {
+			return sizeLeft;
+		}
 
-			return output;
+		@Override
+		public int characteristics() {
+			return SIZED | IMMUTABLE;
 		}
 	}
 }
